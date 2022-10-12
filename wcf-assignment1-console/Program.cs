d854d7d6bcb282f5c3b4878724e42694cf2ceb52
Varshil0010 <Vpatel2996@conestogac.on.ca>
2022-10-12 11:52:27 -0400

    Prime, sum and reverse done!

---
+++ wcf-assignment1-console/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcf_assignment1_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int num1;
+            string num2;
+            string value1;
+            string value2;
+            int number1;
+            int number2;
+
+            //wcfAssignment1.Service1Client service1Client = new wcfAssignment1.Service1Client();
+            wcfAssignment1.Service1Client service1Client = new wcfAssignment1.Service1Client();
+
+            string select = "";
+
+            while (!select.Equals("6"))
+            {
+                Console.WriteLine("WCF Assignment 1");
+                Console.WriteLine("1. Prime Number");
+                Console.WriteLine("2. Sum of digits");
+                Console.WriteLine("3. Reverse a string");
+                Console.WriteLine("4. Print HTML tags");
+                Console.WriteLine("5: Sort 5 numbers");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("\tEnter your choice");
+                select = Console.ReadLine();
+
+                if (select == "1")
+                {
+                    Console.WriteLine("Enter a one number");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+                    num2 = service1Client.primeNumber(num1);
+                    Console.WriteLine(num2);
+
+                }
+                else if (select == "2")
+                {
+                    Console.WriteLine("Enter a number");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+
+                    num2 = service1Client.sumOfDigits(num1);
+                    Console.WriteLine(num2);
+                }
+                else if (select == "3")
+                {
+                    Console.WriteLine("Enter any string");
+                    value1 = Convert.ToString(Console.ReadLine());
+
+                    value2 = service1Client.reverseString(value1);
+                    Console.WriteLine(value2);
+                }
+                else if (select == "4")
+                {
+                    Console.WriteLine("You select 4");
+                }
+                else if(select == "5")
+                {
+                    Console.WriteLine("Enter five numbers");
+                    number1 = Convert.ToInt32(Console.ReadLine());
+
+                    number2 = service1Client.sortNumber(number1);
+                    Console.WriteLine(number2);
+
+                }
+            }
+
+
+        }
+    }
+}
---
+++ wcf-assignment1/Service1.svc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wcf_assignment1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string primeNumber(int primeNum)
+        {
+            int a;
+
+            for (a = 2; a <= primeNum / 2; a++)
+            {
+                if (primeNum % a == 0)
+                {
+                    break;
+                }
+            }
+            if (a > primeNum / 2)
+            {
+                return "Number is Prime";
+            }
+            else
+            {
+                return "Number is not Prime";
+            }
+        }
+
+        public string sumOfDigits(int sum)
+        {
+            int total = 0;
+            while (sum != 0)
+            {
+                total += sum % 10;
+                sum /= 10;
+            }
+            return "Total is: " + total;
+        }
+
+        public string reverseString(string reverse)
+        {
+            char[] reverseArray = reverse.ToCharArray();
+            string output = String.Empty;
+            for (int i = reverseArray.Length - 1; i > -1; i--)
+            {
+                output += reverseArray[i];
+            }
+            return output;
+        }
+
+        public int sortNumber(int sortNum)
+        {
+            int[] sortNum1 = new int[5];
+
+            // Sort the arr in decreasing order
+            // and return a array
+            sortNum1 = sortNum1.OrderByDescending(c => c).ToArray();
+
+            // print all element of array
+            foreach(int num in sortNum1)
+            {
+                return num;
+            }
+            return sortNum1.Length;
+        }
+    }
+}
+
+
